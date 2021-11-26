d5af710e99698462944247174c7230c9d77b0f18
Hamburger57 <93205979+Hamburger57@users.noreply.github.com>
2021-11-26 14:40:54 -0500

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+while (input.logoIsPressed() == false || (input.buttonIsPressed(Button.A) == false || input.buttonIsPressed(Button.B) == false)) {
+    if (input.logoIsPressed() == true && (input.logoIsPressed() == true || input.buttonIsPressed(Button.B) == false)) {
+    	
+    } else {
+    	
+    }
+}
+basic.forever(function () {
+	
+})
