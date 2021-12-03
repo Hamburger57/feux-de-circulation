f7ae4a75f1fc59791492728833ea948b14ba41c4
Hamburger57 <93205979+Hamburger57@users.noreply.github.com>
2021-12-03 10:13:21 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,14 +3,15 @@ function jaune () {
     pins.digitalWritePin(DigitalPin.P1, 1)
     pins.digitalWritePin(DigitalPin.P2, 0)
 }
-function orange2 () {
-    pins.digitalWritePin(DigitalPin.P8, 1)
+function orange_2 () {
     pins.digitalWritePin(DigitalPin.P8, 0)
 }
 function blanc () {
-    pins.digitalWritePin(DigitalPin.P16, 0)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    pins.digitalWritePin(DigitalPin.P8, 0)
 }
 function orange () {
+    pins.digitalWritePin(DigitalPin.P16, 0)
     pins.digitalWritePin(DigitalPin.P8, 1)
 }
 function rouge () {
@@ -24,44 +25,47 @@ function vert () {
     pins.digitalWritePin(DigitalPin.P2, 0)
 }
 basic.forever(function () {
-    vert()
-    basic.pause(5000)
-    jaune()
-    basic.pause(2000)
-    rouge()
-    if (input.buttonIsPressed(Button.A)) {
-        blanc()
-        basic.showLeds(`
-            # . # # .
-            # # . . #
-            # # . . #
-            # # . . #
-            # . # # .
-            `)
-        basic.pause(1000)
-        basic.showNumber(9)
-        basic.pause(1000)
-        basic.showNumber(8)
-        basic.pause(1000)
-        basic.showNumber(7)
-        basic.pause(1000)
-        basic.showNumber(6)
-        basic.pause(1000)
-        orange2()
-        basic.showNumber(5)
-        basic.pause(1000)
-        basic.showNumber(4)
-        basic.pause(1000)
-        basic.showNumber(3)
-        basic.pause(1000)
-        basic.showNumber(2)
-        basic.pause(1000)
-        basic.showNumber(1)
-        basic.pause(1000)
-        basic.showNumber(1)
-        basic.clearScreen()
-        orange()
-    } else {
+    while (!(input.buttonIsPressed(Button.A))) {
+        vert()
+        basic.pause(5000)
+        jaune()
+        basic.pause(2000)
+        rouge()
         basic.pause(5000)
     }
+    rouge()
+    blanc()
+    basic.showLeds(`
+        # . # # .
+        # # . . #
+        # # . . #
+        # # . . #
+        # . # # .
+        `)
+    basic.pause(1000)
+    basic.showNumber(9)
+    basic.pause(1000)
+    basic.showNumber(8)
+    basic.pause(1000)
+    basic.showNumber(7)
+    basic.pause(1000)
+    basic.showNumber(6)
+    basic.pause(1000)
+    basic.showNumber(5)
+    basic.pause(1000)
+    basic.showNumber(4)
+    basic.pause(1000)
+    orange()
+    basic.showNumber(3)
+    basic.pause(1000)
+    orange_2()
+    basic.showNumber(2)
+    basic.pause(1000)
+    orange()
+    basic.showNumber(1)
+    basic.pause(1000)
+    orange_2()
+    basic.showNumber(0)
+    basic.clearScreen()
+    orange()
 })
