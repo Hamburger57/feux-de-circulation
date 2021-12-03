233e8503852d64f534165ea97e9237bc8acc64a7
Hamburger57 <93205979+Hamburger57@users.noreply.github.com>
2021-12-03 10:22:52 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
+let pieton = 0
 function jaune () {
     pins.digitalWritePin(DigitalPin.P0, 0)
     pins.digitalWritePin(DigitalPin.P1, 1)
     pins.digitalWritePin(DigitalPin.P2, 0)
 }
+input.onButtonPressed(Button.A, function () {
+    pieton = 1
+})
 function orange_2 () {
     pins.digitalWritePin(DigitalPin.P8, 0)
 }
@@ -25,47 +29,48 @@ function vert () {
     pins.digitalWritePin(DigitalPin.P2, 0)
 }
 basic.forever(function () {
-    while (!(input.buttonIsPressed(Button.A))) {
-        vert()
-        basic.pause(5000)
-        jaune()
-        basic.pause(2000)
-        rouge()
-        basic.pause(5000)
-    }
+    vert()
+    basic.pause(5000)
+    jaune()
+    basic.pause(2000)
     rouge()
-    blanc()
-    basic.showLeds(`
-        # . # # .
-        # # . . #
-        # # . . #
-        # # . . #
-        # . # # .
-        `)
-    basic.pause(1000)
-    basic.showNumber(9)
-    basic.pause(1000)
-    basic.showNumber(8)
-    basic.pause(1000)
-    basic.showNumber(7)
-    basic.pause(1000)
-    basic.showNumber(6)
-    basic.pause(1000)
-    basic.showNumber(5)
-    basic.pause(1000)
-    basic.showNumber(4)
-    basic.pause(1000)
-    orange()
-    basic.showNumber(3)
-    basic.pause(1000)
-    orange_2()
-    basic.showNumber(2)
-    basic.pause(1000)
-    orange()
-    basic.showNumber(1)
-    basic.pause(1000)
-    orange_2()
-    basic.showNumber(0)
-    basic.clearScreen()
-    orange()
+    basic.pause(5000)
+    if (pieton == 1) {
+        rouge()
+        blanc()
+        basic.showLeds(`
+            # . # # .
+            # # . . #
+            # # . . #
+            # # . . #
+            # . # # .
+            `)
+        basic.pause(1000)
+        basic.showNumber(9)
+        basic.pause(1000)
+        basic.showNumber(8)
+        basic.pause(1000)
+        basic.showNumber(7)
+        basic.pause(1000)
+        basic.showNumber(6)
+        basic.pause(1000)
+        basic.showNumber(5)
+        basic.pause(1000)
+        basic.showNumber(4)
+        basic.pause(1000)
+        orange()
+        basic.showNumber(3)
+        basic.pause(1000)
+        orange_2()
+        basic.showNumber(2)
+        basic.pause(1000)
+        orange()
+        basic.showNumber(1)
+        basic.pause(1000)
+        orange_2()
+        basic.showNumber(0)
+        basic.clearScreen()
+        orange()
+        pieton = 0
+    }
 })
